0a571172f9f02deda7d1252ac146d5ac384b70a7
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2014-12-08 17:37:20 +1100

    my second commit

--- GitProject/GitProject/ViewController.m
+++ GitProject/GitProject/ViewController.m
@@ -13,7 +13,9 @@
 @end
 
 @implementation ViewController
-
+-(void)test{
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
